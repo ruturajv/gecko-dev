61bfb873df5ef75ea44039fc1443822d990f3f78
Ruturaj K. Vartak <ruturaj@gmail.com>
2017-02-13 12:39:46 +0530

    browser_register_actor.js done

--- devtools/server/tests/browser/browser_register_actor.js
+++ devtools/server/tests/browser/browser_register_actor.js
@@ -1,9 +1,11 @@
+"use strict";
+
 var gClient;
 
 function test() {
   waitForExplicitFinish();
-  var {ActorRegistryFront} = require("devtools/shared/fronts/actor-registry");
-  var actorURL = "chrome://mochitests/content/chrome/devtools/server/tests/mochitest/hello-actor.js";
+  let {ActorRegistryFront} = require("devtools/shared/fronts/actor-registry");
+  let actorURL = "chrome://mochitests/content/chrome/devtools/server/tests/mochitest/hello-actor.js";
 
   if (!DebuggerServer.initialized) {
     DebuggerServer.init();
@@ -13,56 +15,50 @@ function test() {
   gClient = new DebuggerClient(DebuggerServer.connectPipe());
   gClient.connect()
     .then(() => gClient.listTabs())
-    .then(aResponse => {
-
-      var options = {
+    .then(response => {
+      let options = {
         prefix: "helloActor",
         constructor: "HelloActor",
         type: { tab: true }
       };
 
-      var registry = ActorRegistryFront(gClient, aResponse);
+      let registry = ActorRegistryFront(gClient, response);
       registry.registerActor(actorURL, options).then(actorFront => {
-        gClient.listTabs(response => {
-          var tab = response.tabs[response.selected];
+        gClient.listTabs(response2 => {
+          let tab = response2.tabs[response2.selected];
           ok(!!tab.helloActor, "Hello actor must exist");
 
           // Make sure actor's state is maintained across listTabs requests.
-          checkActorState(tab.helloActor, () => {
-
-            // Clean up
-            actorFront.unregister().then(() => {
-              gClient.close().then(() => {
-                DebuggerServer.destroy();
-                gClient = null;
-                finish();
-              });
-            });
-          });
+          checkActorState(tab.helloActor, checkActorCleanup.bind(this, actorFront));
         });
       });
     });
 }
 
+function checkActorCleanup(actorFront) {
+  // Clean up
+  actorFront.unregister().then(() => {
+    gClient.close().then(() => {
+      DebuggerServer.destroy();
+      gClient = null;
+      finish();
+    });
+  });
+}
+
 function checkActorState(helloActor, callback) {
   getCount(helloActor, response => {
-    ok(!response.error, "No error");
-    is(response.count, 1, "The counter must be valid");
+    countCheck(1, null, response);
 
-    getCount(helloActor, response => {
-      ok(!response.error, "No error");
-      is(response.count, 2, "The counter must be valid");
+    getCount(helloActor, countResponse => {
+      countCheck(2, null, countResponse);
 
-      gClient.listTabs(response => {
-        var tab = response.tabs[response.selected];
+      gClient.listTabs(listTabsResponse => {
+        let tab = listTabsResponse.tabs[listTabsResponse.selected];
         is(tab.helloActor, helloActor, "Hello actor must be valid");
 
-        getCount(helloActor, response => {
-          ok(!response.error, "No error");
-          is(response.count, 3, "The counter must be valid");
-
-          callback();
-        });
+        // The countCheck will get called with 3rd argument as the response
+        getCount(helloActor, countCheck.bind(this, 3, callback));
       });
     });
   });
@@ -73,4 +69,13 @@ function getCount(actor, callback) {
     to: actor,
     type: "count"
   }, callback);
+}
+
+function countCheck(count, callback, response) {
+  ok(!response.error, "No error");
+  is(response.count, count, "The counter must be valid");
+
+  if (typeof callback === "function") {
+    callback();
+  }
 }
